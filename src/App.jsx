8aa7c8723218ad8c511d341f9bd1df0fa1fd81f5
laura2ndrea <laura2ndrea12@gmail.com>
2025-03-28 12:59:58 -0500

    feat: add json data for displaying information on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import UserCard from "./components/UserCard";
+import users from "./data/users.json";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
 import { faBell, faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
+  const user = users.find((u) => u.id == 1)
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray text-[#343A3F] font-red-hat-display">
       <h1 className="text-red-500 text-2xl font-bold mb-4">Probando componentes 🚀</h1>
       <UserCard 
         className="flex-row-reverse"
-        image="src/assets/icons/photo.jpg" 
-        name="Andrés Vargas"
+        image={user.image}
+        name={user.name}
       />
     </div>
   )
