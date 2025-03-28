9c30ebca49caef448dd68440bfe958f0ccb7afdc
laura2ndrea <laura2ndrea12@gmail.com>
2025-03-28 10:26:11 -0500

    feat(icons): add Icon component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import { Button } from './components/Button'
+import { useState } from 'react';
+import { Icon } from "./components/Icon";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray">
       <h1 className="text-red-500 text-2xl font-bold mb-4">Probando componentes 🚀</h1>
-      <Button text="¡Haz clic!"/>
+      <Icon icon={faEnvelope} className="text-red-500"/>
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 // En la página principal definir color de fondo, texto, color de texto y la redondez de los bordes
 export function Button({ text, className  }) {
     return (
-        <button className={`${className} px-4 py-2 bg-[#90b687] transition duration-700 hover:opacity-70`}>
+        <button className={`${className} px-4 py-2 bg-[#90b687] transition-transform duration-700 hover:opacity-70`}>
             {text}
         </button>
     )
