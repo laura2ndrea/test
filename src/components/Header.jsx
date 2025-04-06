09df549b4fa20d68d08c9cdfdcad541a8374d35a
laura2ndrea <laura2ndrea12@gmail.com>
2025-04-06 10:10:31 -0500

    feat(usercard): improve styles in the component UserCard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import SearchBar from "./SearchBar";
 
-export default function Header({ className }) {
+export default function Header({ className="" }) {
     return (
         <header className={`flex flex-col-reverse justify-center p-1 bg-blue-500 w-full ${className}`}>
             <SearchBar placeholder="Buscar por libros, personas o artículos" className="basis-1/2 md:basis-3/6 md:h-1/2"/>
-            <div className="basis-1/2 md:basis-1/4 bg-yellow-500">
-                Iconos
+            <div className="flex basis-1/2 p-2 px-4 items-center justify-end md:basis-1/4 bg-yellow-500">
+                <div>
+                    <FontAwesomeIcon icon={faBell} className="mr-2 bg-[#FBF2E5] text-secondary-color p-[6px] rounded-lg" />
+                    <FontAwesomeIcon icon={faEnvelope} className="bg-[#E3F4E9] text-primary-color p-[6px] rounded-lg" />
+                </div>
             </div>
         </header>
     );
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,7 @@
-export default function UserCard({ image, name, text, className}) {
+export default function UserCard({ image, name, text, className=""}) {
     return (
-        <div className={`${className} flex items-center text-xs`}>
-            <img src={image} alt={name} className="w-9 h-9 rounded-xl m-2 mt-0 shadow-lg"/>
+        <div className={`flex items-center text-xs gap-x-2 ${className} `}>
+            <img src={image} alt={name} className="w-9 h-9 rounded-xl shadow-lg"/>
             <div>
                 <h3 className="font-bold">{name}</h3>
                 {text && <p className="font-light text-[10px]">{text}</p>}
