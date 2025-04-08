83d7238b148f034806d5a30a318ececfcdc58b81
laura2ndrea <laura2ndrea12@gmail.com>
2025-04-08 09:10:00 -0500

    feat(latest): improve styles in the Latest component

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -3,28 +3,19 @@ import InfoCard from "./InfoCard";
 
 export default function Latest({ data }) {
     return (
-        <section className="flex flex-col w-[18%] h-[90%] bg-[#F2F2F2] overflow-hidden mr-2 rounded-xl text-[#343A3F] shadow-lg">
+        <section className="flex flex-col w-full bg-backgray-color mr-2 rounded-xl text-tertiary-color shadow-lg">
             <div className="flex justify-between m-4 mb-2">
                 <h1 className="text-lg font-bold">Lo último</h1>
-                <Button text="Ver todo" className="rounded-full text-[10px] text-[#7DA870] bg-[#e6eee2]"/>
+                <Button variant="tertiary">Ver todo</Button>
             </div>
-            <ul className="flex mx-4 mb-1 text-xs leading-loose font-extralight">
+            <ul className="flex mx-4 mb-1 text-xs leading-loose font-extralight" role="tablist">
                 <li className="mr-6 cursor-pointer transition-all duration-500 hover:font-bold hover:before:content-['>']"><a href="#"> Reciente</a></li>
                 <li className="cursor-pointer transition-all duration-500 hover:font-bold hover:before:content-['>']"><a href="#"> Popular</a></li>
             </ul>
-            <div className="flex flex-col items-center">
-            {data.slice(0,2).map(({ id, imageBook, title, review, author, imageAuthor, date}) => (
-                <InfoCard 
-                    key={id} 
-                    imageBook={imageBook} 
-                    title={title} 
-                    review={review} 
-                    author={author} 
-                    imageAuthor={imageAuthor} 
-                    date={date}
-                    className="h-[42%] m-2"
-                />
-            ))}
+            <div className="flex flex-col items-center overflow-y-auto px-2">
+                {data.slice(0, 2).map((item) => (
+                    <InfoCard key={item.id} {...item} className="m-2" />
+                ))}
             </div>
         </section>
     );
