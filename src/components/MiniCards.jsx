4480d41bd1b9bda54f27e9d32ba1d82fdf9f8cab
laura2ndrea <laura2ndrea12@gmail.com>
2025-04-08 10:21:00 -0500

    fix(layout): adjust responsive layout for very small screens

--- src/components/MiniCards.jsx
+++ src/components/MiniCards.jsx
@@ -1,6 +1,6 @@
 function MiniCard({ title, text, className="" }) {
     return (
-        <div className={`flex flex-col w-[20%] aspect-square justify-center border-[0.5px] border-gray-300 rounded-xl shadow-lg p-3 text-tertiary-color bg-white ${className}`}>
+        <div className={`flex flex-col w-full justify-center border-[0.5px] border-gray-300 rounded-xl shadow-lg p-3 text-tertiary-color bg-white ${className}`}>
             <h1 className="text-xs font-thin">{title}</h1>
             <p className="font-semibold text-xl">{text}</p>
         </div>
@@ -15,7 +15,7 @@ export default function MiniCards({ data, className="" }) {
         opinions: "Opiniones"
     };
     return (
-        <section className={`flex flex-wrap w-full h-fit justify-between content-center ${className}`}>
+        <section className={`grid grid-cols-2 gap-2 w-full ${className}`}>
             {Object.entries(data).map(([key, value], index) => (
                 <MiniCard key={key} title={labels[key]} text={`${value}${index > 0 ? "k":""}`}/>
             ))}
